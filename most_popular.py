627c3734d95e31052d6ec8a78ae00b2cf703a4b8
sarthak30 <sarthaknsit@gmail.com>
2016-08-11 10:20:07 +0530

    Extracting most popular Retweets for hashtag

--- most_popular.py
+++ most_popular.py
@@ -2,6 +2,7 @@ import credential
 import twitter
 import json
 from collections import Counter
+from prettytable import PrettyTable
 
 def lexical_diversity(tokens):
     return 1.0*len(set(tokens))/len(tokens)
@@ -36,12 +37,13 @@ for i in xrange(5):
     search_results = twitter_api.search.tweets(**kwargs)
     statss = statss + search_results['statuses']
 
-text_status = [status['text'] for status in statss]
-words = [w for t in text_status for w in t.split()]
-screen_name = [user_mention['screen_name'] for status in statss for user_mention in status['entities']['user_mentions']]
-hashtags = [hashtag['text'] for status in statss for hashtag in status['entities']['hashtags']]
+rt = list(set([
+    (status['retweet_count'], status['retweeted_status']['user']['screen_name'], status['text'])
+    for status in statss
+        if(status.has_key('retweeted_status'))
+    ]))
 
-print lexical_diversity(words)
-print lexical_diversity(screen_name)
-print lexical_diversity(hashtags)
-print average_words(text_status)
+pt = PrettyTable(field_names=['Count', 'Screen Names', 'Text'])
+[pt.add_row(row) for row in sorted(rt, reverse=True)[:5]]
+pt.max_width['Text'] = 50
+print pt
